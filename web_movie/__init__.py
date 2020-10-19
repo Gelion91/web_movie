630950e87fb9a4b0a13f76977b6957a780016827
Roman <gelion91@mail.ru>
2020-10-19 13:29:36 +0300

    Исправил отображение фильмов в "категориях"

--- web_movie/__init__.py
+++ web_movie/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
-
 from db import db
 from .user.models import User
 from .user.views import blueprint as user_blueprint
@@ -18,7 +17,6 @@ def create_app():
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'user.login'
-
     app.register_blueprint(user_blueprint)
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(video_blueprint)
